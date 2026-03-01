0af7957e3d77b6d4aabe9d4d707422fd59bd8e92
Nishant Pawar <nishant72003@gmail.com>
2026-03-01 16:47:39 +0530

    Updated name to Kaushalyaa Swap

--- components/homepage.tsx
+++ components/homepage.tsx
@@ -28,7 +28,7 @@ export function Navbar() {
         {/* Logo Section */}
         <Link href="/" className="flex items-center gap-2 font-bold text-xl tracking-tight text-primary hover:opacity-80 transition-opacity">
           <Image src="/logo.jpeg" alt="Logo" width={32} height={32} className="rounded-md" />
-          <span>KaushalyaSwap</span>
+          <span>Kaushalyaa Swap</span>
         </Link>
 
         {/* Navigation Links */}
